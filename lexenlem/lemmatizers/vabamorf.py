6e5550ac273b3dc5bdb8ee874a58b7e1e3226d23
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-04-07 15:47:33 +0300

    Update vabamorf interface

--- lexenlem/lemmatizers/vabamorf.py
+++ lexenlem/lemmatizers/vabamorf.py
@@ -1,4 +1,11 @@
 from estnltk import Text
 
-def lemmatize(token):
-    return Text(token).lemmas[0].split('|')
+
+def lemmatize(token: str) -> str:
+    text = Text(token)
+    text.tag_layer("morph_analysis")
+    lemma: str = text["morph_analysis"][0].lemma[0]
+    return lemma
+
+# def lemmatize(token):
+#     return Text(token).lemmas[0].split('|')
