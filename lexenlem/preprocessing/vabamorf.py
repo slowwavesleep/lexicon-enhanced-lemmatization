ccdc89c2f6d9957e3289afa10077abad073aae54
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-04-14 12:39:42 +0300

    Update adhoc preprocessing

--- lexenlem/preprocessing/vabamorf.py
+++ lexenlem/preprocessing/vabamorf.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple
+from dataclasses import dataclass
+from typing import List, Union
 
 from estnltk import Text
 from estnltk.taggers import VabamorfTagger
@@ -8,22 +9,49 @@ from lexenlem.models.common.vabamorf2conll import neural_model_tags
 tagger = VabamorfTagger(compound=True, disambiguate=False, guess=False)
 
 
+@dataclass
+class BaseAnalysis:
+    token: str
+    lemma: str
+    part_of_speech: str
+
+
+@dataclass
+class VabamorfAnalysis(BaseAnalysis):
+    feats: str
+
+
+@dataclass
+class VabamorfAnalysisConll(BaseAnalysis):
+    feats: str
+
+
 # add lemmas?
-def get_vabamorf_tags(token: str) -> List[Tuple[str, str]]:
+def get_vabamorf_analysis(token: str) -> List[VabamorfAnalysis]:
     text = Text(token)
     text.tag_layer(tagger.input_layers)
     tagger.tag(text)
+    lemmas = text["morph_analysis"][0].root
     forms = text["morph_analysis"][0].form
     parts_of_speech = text["morph_analysis"][0].partofspeech
-    return list(zip(parts_of_speech, forms))
+    return [
+        VabamorfAnalysis(token=token, lemma=lemma, part_of_speech=part_of_speech, feats=form)
+        for lemma, part_of_speech, form in zip(lemmas, parts_of_speech, forms)
+    ]
 
 
-def get_conll_tags(token: str) -> List[List[str]]:
-    vb_tags: List[Tuple[str, str]] = get_vabamorf_tags(token)
-    results = []
-    for pos, form in vb_tags:
-        results.append(neural_model_tags(token, pos, form))
-    return results
+def convert_vb_to_conll(vb_analysis: VabamorfAnalysis) -> List[VabamorfAnalysisConll]:
+    # conversion from vb to conll may be ambiguous
+    candidates: List[str] = neural_model_tags(vb_analysis.token, vb_analysis.part_of_speech, vb_analysis.feats)
+    return [
+        VabamorfAnalysisConll(
+            token=vb_analysis.token,
+            lemma=vb_analysis.lemma,
+            part_of_speech=vb_analysis.part_of_speech,
+            feats=feats_candidate,
+        )
+        for feats_candidate in candidates
+    ]
 
 
 def tokenize(raw_text: str) -> List[str]:
@@ -35,10 +63,15 @@ def tokenize(raw_text: str) -> List[str]:
     return tokenized
 
 
-def basic_preprocessing(raw_text: str):
+def basic_preprocessing(
+        raw_text: str, convert_to_conll: bool = True
+) -> List[Union[VabamorfAnalysis, VabamorfAnalysisConll]]:
     tokenized = tokenize(raw_text)
-    result = []
+    analyzed = []
     for token in tokenized:
-        result.append((token, get_conll_tags(token)[0][0]))
-    return result
+        token_analysis = get_vabamorf_analysis(token)[0]
+        if convert_to_conll:
+            token_analysis = convert_vb_to_conll(token_analysis)[0]
+        analyzed.append(token_analysis)
+    return analyzed
 
