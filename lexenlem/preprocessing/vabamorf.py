088f0550c3c042c9871040576ecf4e074eb1fc4c
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-04-14 16:35:20 +0300

    Update adhoc processing

--- lexenlem/preprocessing/vabamorf.py
+++ lexenlem/preprocessing/vabamorf.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import List, Union
 
+import torch
 from estnltk import Text
 from estnltk.taggers import VabamorfTagger
 from tqdm.auto import tqdm
@@ -9,7 +10,9 @@ import lexenlem.models.common.seq2seq_constant as constant
 from lexenlem.models.common.seq2seq_model import Seq2SeqModelCombined
 from lexenlem.models.common.vabamorf2conll import neural_model_tags
 from lexenlem.models.lemma import edit
+from lexenlem.models.lemma.data import DataLoaderCombined
 from lexenlem.models.lemma.vocab import MultiVocab, Vocab
+from lexenlem.models.common.data import get_long_tensor, sort_all
 
 tagger = VabamorfTagger(compound=True, disambiguate=False, guess=False)
 
@@ -36,6 +39,27 @@ class AdHocInput:
     src_input: List[int]
     lemma_input: List[int]
     edit_type: int
+
+
+@dataclass
+class AdHocModelInput:
+    src: torch.tensor
+    src_mask: torch.tensor
+    lem: torch.tensor
+    lem_mask: torch.tensor
+    orig_idx: List[int]
+
+    def cuda(self):
+        self.src.cuda()
+        self.src_mask.cuda()
+        self.lem.cuda()
+        self.lem_mask.cuda()
+
+    def cpu(self):
+        self.src.cpu()
+        self.src_mask.cpu()
+        self.lem.cpu()
+        self.lem_mask.cpu()
 
 
 def get_vabamorf_analysis(token: str) -> List[VabamorfAnalysis]:
@@ -87,6 +111,87 @@ def basic_vb_preprocessing(
     return analyzed
 
 
+def prepare_batch(
+        preprocessed_input: List[Union[VabamorfAnalysis, VabamorfAnalysisConll]],
+        eos_after: bool,
+        use_pos: bool,
+        use_feats: bool,
+        skip_lemma: bool,
+        vocab: Vocab,
+) -> List[AdHocInput]:
+
+    batch = []
+
+    for element in tqdm(preprocessed_input, desc="Preparing raw batch..."):
+
+        if isinstance(element, VabamorfAnalysis):
+            raise NotImplementedError("Vabamorf output format not supported at the moment.")
+
+        edit_type: int = edit.EDIT_TO_ID[edit.get_edit_type(word=element.token, lemma=element.lemma)]
+
+        surface_form: List[str] = list(element.token)
+        if eos_after:
+            surface_form = [constant.SOS] + surface_form
+        else:
+            surface_form = [constant.SOS] + surface_form + [constant.EOS]
+
+        part_of_speech: List[str] = [f"POS={element.part_of_speech}"]
+
+        feats: List[str] = element.feats.split("|")
+
+        src_input = []
+        src_input += surface_form
+        if use_pos:
+            src_input += part_of_speech
+        if use_feats:
+            src_input += feats
+        if eos_after:
+            src_input += [constant.EOS]
+
+        if skip_lemma:
+            lemma_input = [constant.SOS, constant.EOS]
+        else:
+            # TODO: account for a list of lemmas
+            lemma_input = [constant.SOS, *list(element.lemma), constant.EOS]
+
+        input_element = AdHocInput(
+            src_input=vocab.map(src_input), lemma_input=vocab.map(lemma_input), edit_type=edit_type
+        )
+
+        batch.append(input_element)
+    return batch
+
+
+def process_batch(raw_batch: List[AdHocInput]) -> AdHocModelInput:
+    raw_batch = [[el.src_input, el.lemma_input] for el in raw_batch]
+
+    batch_size = len(raw_batch)
+    batch = list(zip(*raw_batch))
+    assert len(batch) == 2
+
+    lens = [len(x) for x in batch[0]]
+    batch, orig_idx = sort_all(batch, lens)
+
+    src = batch[0]
+    src = get_long_tensor(src, batch_size)
+    src_mask = torch.eq(src, constant.PAD_ID)
+
+    lem = batch[1]
+    lem = get_long_tensor(lem, batch_size)
+    lem_mask = torch.eq(lem, constant.PAD_ID)
+
+    return AdHocModelInput(src, src_mask, lem, lem_mask, orig_idx)
+
+
+def save_vocab(dataloader: DataLoaderCombined, filename: str):
+    params = {"vocab": dataloader.vocab.state_dict()}
+    torch.save(params, filename)
+
+
+def load_vocab(filename: str) -> MultiVocab:
+    return MultiVocab.load_state_dict(torch.load(filename)["vocab"])
+
+
 class VabamorfAdHocProcessor:
 
     def __init__(
@@ -98,76 +203,44 @@ class VabamorfAdHocProcessor:
             eos_after: bool = False,
             convert_to_conll: bool = True,
             skip_lemma: bool = False,
+            use_cuda: bool = False,
     ):
         self.model = model
+        if use_cuda:
+            self.model.cuda()
         self.vocab: Vocab = vocab["combined"]
         self.use_pos = use_pos
         self.use_feats = use_feats
         self.eos_after = eos_after
         self.convert_to_conll = convert_to_conll
         self.skip_lemma = skip_lemma
+        self.use_cuda = use_cuda
 
-    def prepare_batch(
-            self, preprocessed_input: List[Union[VabamorfAnalysis, VabamorfAnalysisConll]]
-    ) -> List[AdHocInput]:
-
-        batch = []
-
-        for element in tqdm(preprocessed_input, desc="Preparing raw batch..."):
-
-            if isinstance(element, VabamorfAnalysis):
-                raise NotImplementedError("Vabamorf output format not supported at the moment.")
-
-            edit_type: int = edit.EDIT_TO_ID[edit.get_edit_type(word=element.token, lemma=element.lemma)]
-
-            surface_form: List[str] = list(element.token)
-            if self.eos_after:
-                surface_form = [constant.SOS] + surface_form
-            else:
-                surface_form = [constant.SOS] + surface_form + [constant.EOS]
-
-            part_of_speech: List[str] = [f"POS={element.part_of_speech}"]
-
-            feats: List[str] = element.feats.split("|")
-
-            src_input = []
-            src_input += surface_form
-            if self.use_pos:
-                src_input += part_of_speech
-            if self.use_feats:
-                src_input += feats
-            if self.eos_after:
-                src_input += [constant.EOS]
-
-            if self.skip_lemma:
-                lemma_input = [constant.SOS, constant.EOS]
-            else:
-                # TODO: account for a list of lemmas
-                lemma_input = [f"{constant.SOS}{''.join(list(element.lemma))}{constant.EOS}"]
-
-            input_element = AdHocInput(
-                src_input=self.vocab.map(src_input), lemma_input=self.vocab.map(lemma_input), edit_type=edit_type
-            )
-
-            batch.append(input_element)
-        return batch
-
-    def process_batch(self, raw_batch: List[AdHocInput]):
-        batch_size = len(raw_batch)
-        return None
-
-    def lemmatize(self, input_str: str) -> List[str]:
+    def lemmatize(self, input_str: str):
         preprocessed: List[Union[VabamorfAnalysis, VabamorfAnalysisConll]] = basic_vb_preprocessing(
             input_str, convert_to_conll=self.convert_to_conll
         )
 
-        raw_batch = self.prepare_batch(preprocessed)
-        pt_batch = self.process_batch(raw_batch)
+        raw_batch = prepare_batch(
+            preprocessed,
+            use_feats=self.use_feats,
+            use_pos=self.use_pos,
+            skip_lemma=self.skip_lemma,
+            eos_after=self.eos_after,
+            vocab=self.vocab
+        )
 
-        # lemma_candidates: List[List[str]] = ...  # [[lemma1, lemma2, ...], [...], ...]
-        # lemma_candidates: List[str] = ["".join(lemma_list) for lemma_list in lemma_candidates]
-        # batch = []
-        # for (surface_form, pos, feats), lemma_candidate in zip(preprocessed, lemma_candidates):
-        #     ...
-        # output_seqs, _, _ = self.model.predict_greedy(src=..., src_mask=..., lem=..., lem_mask=..., log_attn=False)
-        # return output_seqs
+        pt_batch = process_batch(raw_batch)
+
+        if self.use_cuda:
+            pt_batch.cuda()
+
+        output_seqs, _, _ = self.model.predict_greedy(
+            src=pt_batch.src, src_mask=pt_batch.src_mask, lem=pt_batch.lem, lem_mask=pt_batch.lem_mask, log_attn=False
+        )
+
+        # self.vocab.id2unit
+
+        # trainer.predict
+
+        return output_seqs
