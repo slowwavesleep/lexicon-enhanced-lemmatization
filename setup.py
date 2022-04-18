1c50ec15b9d7d88dc323aff99dd92fff7e46b702
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-04-18 13:23:45 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="lexenlem",
+    version="0.0.1",
+    packages=["lexenlem"],
+    python_requires=">=3.6",
+    install_requires=[
+        "estnltk==1.7.0rc0",
+        "numpy==1.21.2",
+        "torch==1.6.0",
+        "tqdm==4.64.0",
+    ]
+)
